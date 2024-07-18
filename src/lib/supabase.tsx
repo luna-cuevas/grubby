54f39e857f66528ac18ded74506385f86ae3e7a9
Luna Cuevas <68247976+luna-cuevas@users.noreply.github.com>
2024-07-18 12:43:07 -0700

    fix: error with supabase client

--- src/lib/supabase.tsx
+++ src/lib/supabase.tsx
@@ -3,39 +3,35 @@ import { createClient, SupabaseClient } from "@supabase/supabase-js";
 let supabaseClient: SupabaseClient | null = null;
 
 export const useSupabase = () => {
-  if (!supabaseClient) {
-    supabaseClient = createClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-      process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY as string,
-      {
-        auth: {
-          autoRefreshToken: true,
-          detectSessionInUrl: true,
-        },
-        realtime: {
-          heartbeatIntervalMs: 5000,
-        },
-      }
-    );
-  }
+  supabaseClient = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY as string,
+    {
+      auth: {
+        autoRefreshToken: true,
+        detectSessionInUrl: true,
+      },
+      realtime: {
+        heartbeatIntervalMs: 5000,
+      },
+    }
+  );
   return supabaseClient;
 };
 
 export const useSupabaseWithServiceRole = () => {
-  if (!supabaseClient) {
-    supabaseClient = createClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-      process.env.SUPABASE_SERVICE_ROLE as string,
-      {
-        auth: {
-          autoRefreshToken: true,
-          detectSessionInUrl: true,
-        },
-        realtime: {
-          heartbeatIntervalMs: 5000,
-        },
-      }
-    );
-  }
+  supabaseClient = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    process.env.SUPABASE_SERVICE_ROLE as string,
+    {
+      auth: {
+        autoRefreshToken: true,
+        detectSessionInUrl: true,
+      },
+      realtime: {
+        heartbeatIntervalMs: 5000,
+      },
+    }
+  );
   return supabaseClient;
 };
